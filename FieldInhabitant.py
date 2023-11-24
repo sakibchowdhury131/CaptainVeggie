68a2b8b17d0d1974bdc291dd0fc13050fede7d48
sakibchowdhury131 <sakibchowdhury131@gmail.com>
2023-11-23 22:17:43 -0500

    FieldInhabitant class added

---
+++ FieldInhabitant.py
@@ -0,0 +1,17 @@
+# Author: Sakib Chowdhury
+# Date: Nov 23 2023
+# Description: FieldInhabitant Class
+
+class FieldInhabitant:
+
+    def __init__(self, field_inhabitant):
+        self._field_inhabitant = field_inhabitant
+
+    
+    def setFieldInhabitant(self, field_inhabitant):
+        self._field_inhabitant = field_inhabitant
+
+    def getFieldInhabitant(self):
+        return self._field_inhabitant
+    
+    
