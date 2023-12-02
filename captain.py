091db96c9d807071a547d6d7c7bd90e26b2a191c
Vaddi Gangaraju Hari Kiran <iamharikiran2001@gmail.com>
2023-12-01 21:43:16 -0500

    added getter and setter

--- captain.py
+++ captain.py
@@ -2,15 +2,32 @@
 # Date: Nov 28 2023
 # Description: Captain Class
 
+
 from Creature import Creature
 
 class Captain(Creature):
     def __init__(self, x, y):
-        super().__init__(x, y, 'V')
+        super().__init__(x, y, "V")
         self._veggies_collected = []
 
-    def add_veggie(self, veggie):
+    def addVeggie(self, veggie):
         self._veggies_collected.append(veggie)
 
     def get_veggies_collected(self):
         return self._veggies_collected
+
+    def set_veggies_collected(self, veggies_collected):
+        self._veggies_collected = veggies_collected
+
+    # Other getter/setter functions for x and y
+    def get_x(self):
+        return self._x
+
+    def set_x(self, x):
+        self._x = x
+
+    def get_y(self):
+        return self._y
+
+    def set_y(self, y):
+        self._y = y
