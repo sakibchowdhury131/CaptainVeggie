07103b2759a25368beb3f3bce61c376aa573edd6
Vaddi Gangaraju Hari Kiran <iamharikiran2001@gmail.com>
2023-12-02 19:50:08 -0500

    adding getter and setter for rabbit class also

--- Rabbit.py
+++ Rabbit.py
@@ -2,8 +2,24 @@
 # Date: Nov 28 2023
 # Description: Rabbit Class
 
+# Rabbit.py
+
 from Creature import Creature
 
 class Rabbit(Creature):
     def __init__(self, x, y):
-        super().__init__(x, y, 'R')
+        super().__init__(x, y, "R")
+
+    # Getter and setter functions for x and y
+    def get_x(self):
+        return self._x
+
+    def set_x(self, x):
+        self._x = x
+
+    def get_y(self):
+        return self._y
+
+    def set_y(self, y):
+        self._y = y
+
